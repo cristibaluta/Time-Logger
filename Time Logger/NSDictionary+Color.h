de5a10acbeb3321fbbce32546eac289036e0c534
Baluta Cristian <cristi.baluta@gmail.com>
2013-07-29 09:44:41 +0300

    Added NSDictionary category to return colours

---
+++ Time Logger/NSDictionary+Color.h
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+Color.h
+//  Time Logger
+//
+//  Created by Baluta Cristian on 29/07/2013.
+//  Copyright (c) 2013 Baluta Cristian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Color)
+
+-(NSColor*)colorForKey:(NSString*)appIdentifier;
+
+@end
---
+++ Time Logger/NSDictionary+Color.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Color.m
+//  Time Logger
+//
+//  Created by Baluta Cristian on 29/07/2013.
+//  Copyright (c) 2013 Baluta Cristian. All rights reserved.
+//
+
+#import "NSDictionary+Color.h"
+
+@implementation NSDictionary (Color)
+
+@end
